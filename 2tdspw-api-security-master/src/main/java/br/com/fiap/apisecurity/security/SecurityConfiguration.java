c677e60bff5756258b76d071ade04c25ad1dc7d6
B3rg4r4 <pedrobergara5@gmail.com>
2025-04-04 22:31:36 -0300

    cirando security configuration e service

---
+++ 2tdspw-api-security-master/src/main/java/br/com/fiap/apisecurity/security/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.fiap.apisecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiduration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+        throws Exception{
+        return httpSecurity
+                .csrf( csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                    //cria regra para endpoint especifico
+                    //.requestMatchers(HttpMethod.DELETE, "/map").hasRole("ADMIN")
+                    //.requestMatchers(HttpMethod.Post, "/livro").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .build();
+
+    }
+
+
+}
---
+++ 2tdspw-api-security-master/src/main/java/br/com/fiap/apisecurity/service/AuthService.java
@@ -0,0 +1,2 @@
+package br.com.fiap.apisecurity.service;public class AuthService {
+}
